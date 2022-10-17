7e1112813688b9b3a55298d588fd98492d115a1f
Richard <charichard09@gmail.com>
2022-10-17 16:22:11 -0700

    add an aboutMe object and print info as window.alert()

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,32 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function multiNum(number1, number2) {
+  return number1 * number2;
+}
+
+function subNum(number1, number2) {
+  return number1 - number2;
+}
+
+function divNum(number1, number2) {
+  return number1 / number2;
+}
+
+let aboutMe = {
+  name: "Richard Cha",
+  age: 31,
+  likesTunaFish: true,
+  favoriteSaying: function() {
+    return "SHEEESH";
+  }
+}
+
 //user interface logic
-window.alert(add(parseInt(window.prompt("Enter a number:")), parseInt(window.prompt("Enter another number:"))))
+// const promptNum1 = window.prompt("Please enter number 1:");
+// const promptNum2 = window.prompt("Please enter number 2:");
+
+// window.alert(promptNum1 + " + " + promptNum2 +  " = " + add(parseInt(promptNum1), parseInt(promptNum2)) + ", " + promptNum1 + " - " + promptNum2 + " = " + subNum(promptNum1, promptNum2) + ", " + promptNum1 + " * " + promptNum2 + " = " + multiNum(promptNum1, promptNum2) + ", " + promptNum1 + " / " + promptNum2 + " = " + divNum(promptNum1, promptNum2));
+
+window.alert("My name is " + aboutMe.name + ", age " + String(aboutMe.age) + ". The fact that I like Tuna is "
+             + String(aboutMe.likesTunaFish) + " and my favorite saying is " + aboutMe.favoriteSaying() + "!!")
